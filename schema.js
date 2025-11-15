9fd953c96be356fd0f9c155d707469ed153e7ad7
Moses Okode <mosesokode76@gmail.com>
2025-11-16 02:54:50 +0300

    connect the defined the database

---
+++ schema.js
@@ -0,0 +1,48 @@
+const typeDefs = `#graphql
+type Game {
+    id: ID!,
+    title: String!,
+    platform: [String!]!
+    reviews: [Review!]
+   
+    }
+type Review {
+    id: ID!,
+    rating: Int!,
+    content: String!
+    game: Game!
+    author: Author!
+    
+}
+type Author {
+    id: ID!,
+    name: String!,
+    verified: Boolean!
+    reviews: [Review!]
+    game: Game!
+}
+type Query {
+    reviews: [Review],
+    review(id:ID!): Review,
+    games:[Game],
+    game(id: ID!): Game,
+    authors: [Author],
+    author(id: ID!): Author
+}
+type Mutation {
+    addGame(game: addGameInput): Game
+    deleteGame(id: ID!):[Game]
+    updateGame(id:ID, edit:editGameInput): Game
+}
+input addGameInput {
+    title: String,
+    platform: [String]
+}
+input editGameInput {
+    id: ID,
+    title: String,
+    platform: [String]
+}
+
+`
+export default typeDefs
